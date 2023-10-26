2b02cc1dce14c264de8938a63e1c511ed80eb68e
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-10-26 22:20:48 +0900

    Edit page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+
+interface ApiResponse {
+    result: string;
+}
 
 export default function Home() {
-  return (
-      <div className={styles.container}>
-        <h1>Welcome to Frontend</h1>
-      </div>
-  )
+    const [data, setData] = useState<ApiResponse | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://api.sspzoa.io/predict');
+                const result: ApiResponse = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            {data && <h1>{data.result}</h1>}
+        </div>
+    );
 }
